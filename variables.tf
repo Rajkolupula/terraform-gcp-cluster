26c11338d08c63ce506b562a086e59296776f013
praveenkumar4595-devops <kumarnani4595@gmail.com>
2025-08-18 16:52:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "dockerhub_username" {
+  description = "Docker Hub username"
+  type        = string
+  sensitive   = true
+}
+
+variable "dockerhub_password" {
+  description = "Docker Hub password or access token"
+  type        = string
+  sensitive   = true
+}
