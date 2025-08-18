b41f119ccecb55e4b51c895fe864f0425e0628d9
praveenkumar4595-devops <kumarnani4595@gmail.com>
2025-08-18 15:32:05 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
-output "cluster_name" {
-  description = "The name of the created GKE cluster."
-  value       = google_container_cluster.primary.name
+output "vm_instance_public_ip" {
+  description = "Public IP address of the VM instance"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
 
-# Output the public endpoint of the GKE cluster. This is used to connect to
-# the cluster with tools like kubectl.
-output "cluster_endpoint" {
-  description = "The public endpoint of the GKE cluster."
-  value       = google_container_cluster.primary.endpoint
-  sensitive   = true # Marks the endpoint as sensitive to prevent it from being shown in logs.
+output "vm_instance_ssh_command" {
+  description = "Command to SSH into the VM"
+  value       = "gcloud compute ssh --zone ${google_compute_instance.vm_instance.zone} ${google_compute_instance.vm_instance.name}"
 }
 
-# Output the cluster's Certificate Authority data, which is needed for kubectl.
-output "cluster_ca_certificate" {
-  description = "The cluster's root certificate authority data."
-  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-  sensitive   = true
+output "storage_bucket_url" {
+  description = "URL of the created Google Cloud Storage bucket"
+  value       = "https://console.cloud.google.com/storage/browser/${google_storage_bucket.bucket.name}"
+}
+
+output "docker_installation_status" {
+  description = "Confirmation that Docker/Git were installed via startup script"
+  value       = "Docker, Git, and Docker Compose were installed via metadata_startup_script."
 }
